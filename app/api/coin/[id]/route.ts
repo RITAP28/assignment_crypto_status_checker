15587c91f4e6f6e2c0d1d86a61ac99864fbb1ec1
RITAP28 <ritap2804manutd@gmail.com>
2025-08-03 00:02:30 +0530

    chart functionality added

---
+++ app/api/coin/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  const { searchParams } = new URL(req.url);
+  const vs_currency = searchParams.get("vs_currency") || "usd";
+  const from = searchParams.get("from");
+  const to = searchParams.get("to");
+
+  const coinId = params.id;
+
+  const apiUrl = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart/range?vs_currency=${vs_currency}&from=${from}&to=${to}`;
+
+  try {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Error fetching data", { status: 500 });
+  }
+}
--- app/coin/[id]/page.tsx
+++ app/coin/[id]/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Tooltip,
-  Legend,
-} from "chart.js";
+// import { Line } from "react-chartjs-2";
+// import {
+//   Chart as ChartJS,
+//   LineElement,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
 import { ICoinDataProps } from "@/lib/interfaces";
 import axios from "axios";
 import LoadingDots from "@/lib/loadingDots";
@@ -18,68 +18,126 @@ import Image from "next/image";
 import { useParams } from "next/navigation";
 import { TrendingDown, TrendingUp } from "lucide-react";
 
-ChartJS.register(
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Tooltip,
-  Legend
-);
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const chartConfig = {
+  "1d": {
+    label: "1d",
+    color: "var(--chart-1)",
+  },
+  "7d": {
+    label: "7d",
+    color: "var(--chart-2)",
+  },
+  "30d": {
+    label: "30d",
+    color: "var(--chart-5)",
+  },
+  "90d": {
+    label: "90d",
+    color: "var(--chart-4)",
+  },
+} satisfies ChartConfig;
 
 export default function Page() {
   const params = useParams();
   const [coin, setCoin] = useState<ICoinDataProps | null>(null);
-  const [chartData, setChartData] = useState(null);
-  const [range, setRange] = useState<string>("30");
+  const [chartDataLoading, setChartDataLoading] = useState<boolean>(false);
+  const [chartDataError, setChartDataError] = useState<string | null>(null);
+
+  const [activeChart, setActiveChart] = useState<"1d" | "7d" | "30d" | "90d">(
+    "1d"
+  );
+  const chartDaysMap = {
+    "1d": 1,
+    "7d": 7,
+    "30d": 30,
+    "90d": 90,
+  };
+
+  // chart data state
+  // response data: { prices: {[1754071913341, 114244.25155508265], [1754071913341, 114244.25155508265], more...}, market_caps: {[1754071913341, 114244.25155508265], [1754071913341, 114244.25155508265], more...}, total_volumes: {[1754071913341, 114244.25155508265], [1754071913341, 114244.25155508265], more...} }
+  const [chartData, setChartData] = useState<
+    { date: string; price: number }[] | null
+  >(null);
+
+  function getUnixRange(days: number): { from: number; to: number } {
+    const now = Math.floor(Date.now() / 1000);
+    const secondsInDay = 86400;
+    const from = now - days * secondsInDay;
+    return { from, to: now };
+  }
+
+  const fetchChart = async () => {
+    try {
+      setChartDataLoading(true);
+      setChartDataError(null);
+
+      const days = chartDaysMap[activeChart]; // 1, 7, 30, or 90
+      const { from, to } = getUnixRange(days);
+
+      const res = await axios.get(`/api/coin/${params.id}`, {
+        params: {
+          vs_currency: "usd",
+          from: from,
+          to: to,
+        },
+      });
+      console.log("response data: ", res.data);
+
+      if (res.data) {
+        // Format for recharts: [{ date, price }]
+        const pricesFormatted = res.data.prices.map((entry: number[]) => ({
+          date: new Date(entry[0]).toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+          }),
+          price: entry[1],
+        }));
+
+        setChartData(pricesFormatted);
+      }
+    } catch (error) {
+      console.error("Error while obtaining chart data: ", error);
+      setChartDataError("Something went wrong");
+    } finally {
+      setChartDataLoading(false);
+    }
+  };
+
+  const fetchCoin = async () => {
+    const res = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${params.id}`,
+      {
+        headers: {
+          Accept: "application/json",
+          "x-cg-demo-api-key": process.env.NEXT_PUBLIC_API_KEY,
+        },
+      }
+    );
+    setCoin(res.data);
+  };
 
   useEffect(() => {
-    const fetchCoin = async () => {
-      const res = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/${params.id}`,
-        {
-          headers: {
-            Accept: "application/json",
-            "x-cg-demo-api-key": process.env.NEXT_PUBLIC_API_KEY,
-          },
-        }
-      );
-      setCoin(res.data);
-    };
-
-    const fetchChart = async () => {
-      const res = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/${params.id}/market_chart`,
-        {
-          params: { vs_currency: "usd", days: range },
-          headers: {
-            Accept: "application/json",
-            "x-cg-demo-api-key": process.env.NEXT_PUBLIC_API_KEY,
-          },
-        }
-      );
-      console.log("price: ", res.data);
-      const prices = res.data.prices.map((p: number[]) => ({
-        x: new Date(p[0]).toLocaleDateString(),
-        y: p[1],
-      }));
-      setChartData({
-        labels: prices.map((p) => p.x),
-        datasets: [
-          {
-            label: `Price (USD)`,
-            data: prices.map((p: any) => p.y),
-            fill: false,
-            borderColor: "#3b82f6",
-            tension: 0.1,
-          },
-        ],
-      });
-    };
-
     fetchCoin();
+  }, [params.id]);
+
+  useEffect(() => {
     fetchChart();
-  }, [params.id, range]);
+  }, [params.id, activeChart]);
 
   if (!coin)
     return (
@@ -139,15 +197,15 @@ export default function Page() {
           </div>
 
           <div className="grid grid-cols-2 md:grid-cols-3 gap-2 text-sm">
-            <div className="text-xl bg-zinc-600 px-4 py-2 rounded-md flex items-center">
-              <span className="text-white font-semibold pr-1">Rank:</span>{" "}
-              <span className="font-semibold text-teal-500">
-                #{coin.market_cap_rank}
+            <div className="bg-zinc-600 px-4 py-2 rounded-md flex flex-col items-start">
+              <span className="text-white font-semibold pr-1">Rank</span>{" "}
+              <span className="font-semibold text-teal-500 text-xl">
+                {coin.market_cap_rank}
               </span>
             </div>
-            <div className="text-lg bg-zinc-600 px-4 py-2 rounded-md flex items-center">
-              <span className="text-white font-semibold pr-1">Price:</span>{" "}
-              <span className="font-semibold text-teal-500">
+            <div className="bg-zinc-600 px-4 py-2 rounded-md flex flex-col items-start">
+              <span className="text-white font-semibold pr-1">Price</span>{" "}
+              <span className="font-semibold text-teal-500 text-xl">
                 ${market.current_price.usd.toLocaleString()}
               </span>
             </div>
@@ -190,23 +248,70 @@ export default function Page() {
             <h2 className="text-xl font-semibold mb-2 text-white">
               Price Chart
             </h2>
-            <div className="space-x-2 mb-4">
-              {["1", "7", "30", "90"].map((d) => (
-                <button
-                  key={d}
-                  className={`px-3 py-1 font-semibold rounded-md ${
-                    range === d
-                      ? "bg-teal-500 text-white"
-                      : "bg-zinc-500 text-white"
-                  }`}
-                  onClick={() => setRange(d)}
-                >
-                  {d === "1" ? "24h" : `${d}d`}
-                </button>
-              ))}
+
+            <div className="w-full flex flex-row gap-2 pb-4">
+              {Object.keys(chartConfig).map((key) => {
+                const chart = key as keyof typeof chartConfig;
+                return (
+                  <button
+                    key={chart}
+                    data-active={activeChart === chart}
+                    onClick={() => setActiveChart(chart)}
+                    className={`${
+                      activeChart === chart
+                        ? "bg-teal-500 text-white"
+                        : "bg-zinc-500 text-zinc-400 hover:cursor-pointer hover:bg-zinc-700 transition duration-200 ease-in-out"
+                    } px-4 py-1 rounded-sm font-semibold mx-1`}
+                  >
+                    {chartConfig[chart].label}
+                  </button>
+                );
+              })}
             </div>
-            {chartData ? (
-              <Line data={chartData} />
+
+            {chartDataLoading ? (
+              <div className="w-full h-full flex justify-center items-center">
+                <p className="flex flex-row text-white">
+                  <span className="">Loading your chart</span>
+                  <LoadingDots />
+                </p>
+              </div>
+            ) : chartDataError ? (
+              <div className="w-full h-full flex justify-center items-center">
+                {chartDataError}
+              </div>
+            ) : chartData ? (
+              <ChartContainer
+                config={chartConfig}
+                className="aspect-auto h-[250px] w-full"
+              >
+                <LineChart data={chartData} margin={{ left: 12, right: 12 }}>
+                  <CartesianGrid vertical={false} />
+                  <XAxis
+                    dataKey="date"
+                    tickLine={false}
+                    axisLine={false}
+                    tickMargin={8}
+                    minTickGap={30}
+                  />
+                  <ChartTooltip
+                    content={
+                      <ChartTooltipContent
+                        className="w-[150px]"
+                        nameKey="price"
+                        labelFormatter={(value) => `${value}`}
+                      />
+                    }
+                  />
+                  <Line
+                    dataKey="price"
+                    type="monotone"
+                    stroke={chartConfig[activeChart].color}
+                    strokeWidth={2}
+                    dot={false}
+                  />
+                </LineChart>
+              </ChartContainer>
             ) : (
               <p className="text-sm text-gray-400">Loading chart...</p>
             )}
