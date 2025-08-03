d4f83e276e005bd275bc8c33c798248e7881bddd
RITAP28 <ritap2804manutd@gmail.com>
2025-08-03 17:43:38 +0530

    deploy error fixed 03

--- app/api/coin/[id]/route.ts
+++ app/api/coin/[id]/route.ts
@@ -1,17 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+type Params = Promise<{ id: string }>;
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Params } 
 ) {
   const { searchParams } = new URL(req.url);
   const vs_currency = searchParams.get("vs_currency") || "usd";
   const from = searchParams.get("from");
   const to = searchParams.get("to");
 
-  const coinId = params.id;
+  const { id } = await params;
 
-  const apiUrl = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart/range?vs_currency=${vs_currency}&from=${from}&to=${to}`;
+  const apiUrl = `https://api.coingecko.com/api/v3/coins/${id}/market_chart/range?vs_currency=${vs_currency}&from=${from}&to=${to}`;
 
   try {
     const res = await fetch(apiUrl);
